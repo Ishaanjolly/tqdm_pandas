b8f30a05605e90bc44f97653f24b2d1039461a69
IshaanJolly <ijolly1997@gmail.com>
2025-06-04 20:10:29 +0530

    feat: remove comments and add return types

--- tqdm_pandas/patcher.py
+++ tqdm_pandas/patcher.py
@@ -1,62 +1,48 @@
 
 import pandas as pd
-from tqdm_pandas.reader import (
-    read_csv_with_progress,
-    read_excel_with_progress,
-    read_json_with_progress,
-    read_table_with_progress,
-)
+from tqdm_pandas.reader import create_progress_readers
 import logging 
 logger = logging.getLogger(__name__)
 
 
-_original_functions = {}
+_originals = {}
 
-def patch_pandas():
-    """
-    Monkey patch pandas read functions to include progress bars.
-    This allows users to continue using pd.read_csv() while getting
-    progress bars automatically.
-    """
-    
-    if _original_functions:
+def patch_pandas() -> bool:
+    """Add progress bars to pandas read functions."""
+    if _originals:
         logger.warning("pandas is already patched with tqdm progress bars")
-        return False
+        return False  
+
+    _originals.update({
+        'read_csv': pd.read_csv,
+        'read_excel': pd.read_excel, 
+        'read_json': pd.read_json,
+        'read_table': pd.read_table,
+        'read_parquet': pd.read_parquet
+    })
     
-    # Store original functions and replace with patched versions
-    _original_functions = {
-        "read_csv": pd.read_csv,
-        "read_excel": pd.read_excel,
-        "read_json": pd.read_json,
-        "read_parquet": pd.read_parquet,
-        "read_sql": pd.read_sql,
-        "read_table": pd.read_table,
-    }
+    progress_readers = create_progress_readers(_originals)
     
-    # Replace with tqdm-enabled versions
-    pd.read_csv = read_csv_with_progress
-    pd.read_excel = read_excel_with_progress
-    pd.read_json = read_json_with_progress
-    pd.read_table = read_table_with_progress
+    pd.read_csv = progress_readers['read_csv']
+    pd.read_excel = progress_readers['read_excel']
+    pd.read_json = progress_readers['read_json']
+    pd.read_table = progress_readers['read_table']
+    pd.read_parquet = progress_readers['read_parquet']
+    
+    logger.info("pandas read functions patched with tqdm progress bars")
     
     return True
 
-def unpatch_pandas():
-    """
-    Restore original pandas read functions without progress bars.
-    """
-    
-    if not _original_functions:
+def unpatch_pandas() -> bool:
+    """Restore original pandas read functions."""
+    if not _originals:
         logger.warning("pandas is not currently patched by tqdm_pandas")
-        return False
+        return False 
     
-    # Restore original functions
-    pd.read_csv = _original_functions["read_csv"]
-    pd.read_excel = _original_functions["read_excel"]
-    pd.read_json = _original_functions["read_json"]
-    pd.read_table = _original_functions["read_table"]
+
+    for name, func in _originals.items():
+        setattr(pd, name, func)
     
-    # Clear the stored originals
-    _original_functions.clear()
-    
+    _originals.clear()
+    logger.info("pandas read functions restored to original versions")
     return True
